b22033e9e01fb188d6a2f862ec14565d74c8794b
Milena <milematoss@hotmail.com>
2020-12-15 19:15:09 -0300

    test: validate local part larger than 64 chars

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -15,4 +15,9 @@ describe('Email Validation', () => {
     const email: string = 'any_email@mail.com'
     expect(Email.validate(email)).toBeTruthy()
   })
+
+  test('should not accept local part larger than 64 chars', () => {
+    const email: string = 'l'.repeat(65) + '@mail.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })
