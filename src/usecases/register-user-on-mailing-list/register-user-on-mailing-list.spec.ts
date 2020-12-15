3bafd05782edc8898ee29586ff49cf4d6c84925b
Milena <milematoss@hotmail.com>
2020-12-14 21:49:08 -0300

    test: should add user with complete data to mailing list

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,15 @@
+import { UserData } from './user-data'
+
+describe('Register User on Mailing List use case', () => {
+  test('should add user with complete data to mailing list', async () => {
+    const users: UserData[] = []
+    console.log(users)
+    // const repo: UserRepository = new InMemoryUserRepository(users)
+    // const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    // const name = 'any_name'
+    // const email = 'any_@email.com'
+    // const response = await usecase.registerUserOnMainlingList({ name, email })
+    // const user = repo.findUserByEmail(email)
+    // expect(await user).toBe(name)
+  })
+})
