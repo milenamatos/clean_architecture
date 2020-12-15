a859d96c9bc164b44621f41ec28eb416843c1818
Milena <milematoss@hotmail.com>
2020-12-14 22:29:21 -0300

    refactor: improve variable name in tests

--- src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -4,8 +4,8 @@ import { InMemoryUserRepository } from './in-memory-user-repository'
 describe('In Memory User Repository', () => {
   test('should return null if user is not found', async () => {
     const users: UserData[] = []
-    const userRepo = new InMemoryUserRepository(users)
-    const user = await userRepo.findUserByEmail('any_email@email.com')
+    const sut = new InMemoryUserRepository(users)
+    const user = await sut.findUserByEmail('any_email@email.com')
     expect(user).toBeNull()
   })
 
@@ -13,9 +13,9 @@ describe('In Memory User Repository', () => {
     const users: UserData[] = []
     const name = 'any_name'
     const email = 'any_email@email.com'
-    const userRepo = new InMemoryUserRepository(users)
-    await userRepo.add({ name, email })
-    const user = await userRepo.findUserByEmail('any_email@email.com')
+    const sut = new InMemoryUserRepository(users)
+    await sut.add({ name, email })
+    const user = await sut.findUserByEmail('any_email@email.com')
     expect(user.name).toBe('any_name')
   })
 
@@ -24,8 +24,8 @@ describe('In Memory User Repository', () => {
       { name: 'first_name', email: 'first@email.com' }, {
         name: 'second_name', email: 'second@email.com'
       }]
-    const userRepo = new InMemoryUserRepository(users)
-    const returnedUsers = await userRepo.findAllUsers()
+    const sut = new InMemoryUserRepository(users)
+    const returnedUsers = await sut.findAllUsers()
     expect(returnedUsers.length).toBe(2)
   })
 })
