13ff437dbf3abac53b81c0b7348ff7b7c8ebb683
Milena <milematoss@hotmail.com>
2021-01-19 20:11:06 -0300

    test: improve error classes

--- src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -2,9 +2,6 @@ import { UserData } from '../../entities/user-data'
 import { UserRepository } from './ports/user-repository'
 import { InMemoryUserRepository } from './repository/in-memory-user-repository'
 import { RegisterUserOnMailingList } from './register-user-on-mailing-list'
-import { left } from '../../shared/either'
-import { InvalidEmailError } from '../../entities/errors/invalid-email-error'
-import { InvalidNameError } from '../../entities/errors/invalid-name-error'
 
 describe('Register User on Mailing List use case', () => {
   test('should add user with complete data to mailing list', async () => {
@@ -25,10 +22,10 @@ describe('Register User on Mailing List use case', () => {
     const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
     const name = 'any_name'
     const email = 'invalid_email'
-    const response = await usecase.registerUserOnMailingList({ name, email })
+    const response = (await usecase.registerUserOnMailingList({ name, email })).value as Error
     const user = await repo.findUserByEmail(email)
     expect(user).toBeNull()
-    expect(response).toEqual(left(new InvalidEmailError()))
+    expect(response.name).toEqual('InvalidEmailError')
   })
 
   test('should not add user with invalid name to mailing list', async () => {
@@ -37,9 +34,9 @@ describe('Register User on Mailing List use case', () => {
     const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
     const name = ''
     const email = 'any@email.com'
-    const response = await usecase.registerUserOnMailingList({ name, email })
+    const response = (await usecase.registerUserOnMailingList({ name, email })).value as Error
     const user = await repo.findUserByEmail(email)
     expect(user).toBeNull()
-    expect(response).toEqual(left(new InvalidNameError()))
+    expect(response.name).toEqual('InvalidNameError')
   })
 })
